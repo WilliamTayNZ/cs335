04a990335e0f6546afe0912ac8528ef11abeb3e1
William Tay <williamthomastay@gmail.com>
2026-08-17 17:30:29 +1200

    add L09 examples

---
+++ L09/examples/WebAPIFile/Controllers/FileController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPIImg.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class FileController : Controller
+    {
+        [HttpGet("GetFile/{name}")]
+        public ActionResult GetFile(string name)
+        {
+            string path = Directory.GetCurrentDirectory();
+            string imgDir = Path.Combine(path, "Files");
+            string fileName1 = Path.Combine(imgDir, name + ".png");
+            string fileName2 = Path.Combine(imgDir, name + ".pdf");
+            string respHeader = "";
+            string fileName = "";
+            if (System.IO.File.Exists(fileName1))
+            {
+                respHeader = "image/png";
+                fileName = fileName1;
+            }
+            else if (System.IO.File.Exists(fileName2))
+            {
+                respHeader = "application/pdf";
+                fileName = fileName2;
+            }
+            else
+                return NotFound("File not found");
+            return PhysicalFile(fileName, respHeader);
+        }
+    }
+}
